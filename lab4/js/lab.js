1b869dd58c242cb8341f022759da461777af4fc0
maxlucaschwab <114507212+maxlucaschwab@users.noreply.github.com>
2022-10-14 18:00:11 -0700

    updated lab 5

---
+++ lab4/js/lab.js
@@ -0,0 +1,37 @@
+// <main>
+//   <h1>Task 1: Pseudocode an Everyday Task</h1>
+//   <p>Pseudocoding Gardening</p>
+//   <ol>
+//     <li>Find a place to garden.</li>
+//     <li>Find a spot with good soil.</li>
+//     <li>Gather seeds, garden tools, and fertilizer.</li>
+//     <li>Gather water.</li>
+//     <li>Prepare the soil.</li>
+//     <ol>
+//       <li>Remove weeds.</li>
+//       <li>Dig holes for plants.</li>
+//       <li>Spread fertilizer.</li>
+//     </ol>
+//     <li>Plant the seeds</li>
+//     <ol>
+//       <li>Water the seeds.</li>
+//       <li>Cover the seeds with soil.</li>
+//       <li>Wait for plant to grow.</li>
+//     </ol>
+//   </ol>
+//   <h1>Task 2: Pseudocode a Computer Task</h1>
+//   <p>Pseudocoding a Hangman Game</p>
+//   <ol>
+//     <li>Print game board</li>
+//     <li>Accept user input</li>
+//     <li>Check user input against hidden solution</li>
+//     <ol>
+//       <li>If user input is correct, reveal solution</li>
+//       <li>Else print additional part of hangman</li>
+//       <ol>
+//         <li>If hangman is fully drawn, user loses</li>
+//         <li>Else print additional hangman part</li>
+//       </ol>
+//     </ol>
+//   </ol>
+// </main>
---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ * Author:    Max and Thomas
+ * Created:   10.14.2022
+ * License: Public Domain
+ **/
+
+// defines all the variables
+carMake = "Honda";
+carYear = 2008;
+carModel = "Civic";
+carColor = "Blue";
+
+// age calculation
+age = 2022 - carYear;
+
+// output
+document.writeln("Make: "+ carMake + "<br>");
+document.writeln("Model: "+ carmodel + "<br>");
+document.writeln("Color: " + carColor + "<br>");
+document.writeln("Year: " + carYear + "<br>");
+document.writeln("Age: " age + " years <br>");
