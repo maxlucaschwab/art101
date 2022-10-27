0f2bab3047376d8d947bf0b12a7e2f6177d31ad8
maxlucaschwab <114507212+maxlucaschwab@users.noreply.github.com>
2022-10-26 17:52:27 -0700

    Updated function

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -7,8 +7,22 @@
  // function sortUserName sorts a user input and returns it jumbled
 function sortUserName() {
    var userName = window.prompt("Hello! Please enter your name so we can fix it.");
-   return userName.split('').sort().join('');
- }
+   var nameArray = userName.split(" ");
+   newNameArray = [];
+
+   // Splits the original string into individual words before sorting the words themselves
+   for (var i = 0; i < nameArray.length; i++) {
+     newNameArray[i] = nameArray[i].split('').sort().reverse().join('');
+
+     if (i != nameArray.length){
+       newNameArray[i] = (newNameArray[i] + " ");
+     };
+   };
+
+   // return nameArray.sort().join('');
+   return newNameArray.sort().reverse().join('').toLowerCase();
+ };
+// sorts the new list with scrambled words, scrambling the new words as well
 
 // Output
 document.writeln("Oh hey, I've fixed your name: ",
